5273a7082e86291093276c53bb729afe61ddb615
Emma <em.smyth5@gmail.com>
2024-01-16 16:45:35 +0000

    set up global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check In</Button>
+        <Button onClick={() => alert("Check out")}>Check Out</Button>
+
+        <Input type="number" placeholder="Number of Guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
