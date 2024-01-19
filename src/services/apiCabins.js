b94ea29a6794a1bc64dd5add1bf14f9cf2c4b4ee
Emma <em.smyth5@gmail.com>
2024-01-19 21:59:33 +0000

    delete a cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,8 +4,19 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.error("Cabins could not be loaded");
+    console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
   }
 
   return data;
